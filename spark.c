6bcaa28186b748771c7cd6bddc5b0fd71ea6ba44
vaishnavi9850 <121082141+vaishnavi9850@users.noreply.github.com>
2022-12-20 12:06:01 +0530

    Update spark.c

--- spark.c
+++ spark.c
@@ -1,5 +1,16 @@
 #include<stdio.h>
+void add();
 main()
 {
 printf("\nWelcome");
+  printf("\n spark institute");
+  add();
+}
+void add()
+{
+int a,b,c;
+  printf("\nEnter a & b");
+  scanf("%d%d",&a,&b);
+  c=a+b;
+  printf("\nadd=%d",c);
 }
